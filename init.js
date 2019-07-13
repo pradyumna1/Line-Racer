0a46f093cf73b7f8b77be8b3ccc5c60e715cbbb3
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-07-13 03:55:30 +0000

    direct stdout of children to parent in init.js

--- init.js
+++ init.js
@@ -2,21 +2,24 @@ const cp = require('child_process');
 
 const racerCount = process.argv[2];
 
-const masterOptions = {
-    detached: true,
-    stdio: [process.stdin, process.stdout, process.stderr],
-};
+if (!racerCount) {
+    console.log('Please give racerCount: node init.js racerCount');
+    process.exit(-1);
+} 
 
-const master = cp.spawn('node', ['Master.js']);
-master.unref()
-console.log('Started master with id ', master.pid);
-const racerOptions = {
+const childOptions = {
     detached: true,
     stdio: 'inherit',
-}
+};
+
+const master = cp.spawn('node', ['Master.js', racerCount], childOptions);
+console.log('Started master with id ', master.pid);
+master.unref();
+
 for (let i = 0; i < racerCount; i++) {
-    const racer = cp.spawn('node', ['Racer.js', i + 1]);
-    racer.unref()
+    const racer = cp.spawn('node', ['Racer.js', i + 1], childOptions);
     console.log(`Started Racer ${i + 1} with pid ${racer.pid}`);
+    racer.unref();
 }
+
 process.exit(0);
