51f3685e7713164144390f1d6224a2fd02e7434d
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-07-13 04:37:25 +0000

    Added updatePos utility

--- Master.js
+++ Master.js
@@ -4,24 +4,50 @@ ipc.config.id = 'master'
 ipc.config.retry = 15000;
 ipc.config.silent = true;
 
-let racersStarted = 0;
+let registeredRacers = 0;
 const racerCount = process.argv[2];
 const racers = {};
+
+function getLine() {
+    return [Math.random() * 100, Math.random() * 100];
+}
+
+function* getLines(lineCount) {
+    if (lineCount === 0) {
+        return;
+    }
+    yield getLine();
+    yield* getLines(lineCount--);
+}
+
+function beginLap() {
+    const currentLap = 
+    ipc.server.broadcast('lap.begin', {lines: [...getLines(racerCount)]});
+}
+
+function updatePosition({id, coords}, socket) {
+    racers[id].coords = coords;
+    
+}
+
+function registerRacer(racer, socket) {
+    if (racers[racer.id] == null && racerCount !== racersStarted) {
+        racers[racer.id] = {
+            socket,
+        };
+        registersRacers++;
+        console.log('Racer registered : ', racer);
+        if (registeredRacers === racerCount) {
+            beginLap();
+        }
+    }
+}
+
 ipc.serve(
     function () {
-        ipc.server.on(
-            'racer.init',
-            function (data, socket) {
-                console.log('Racer registered : ', data);
-                if (racers[data.racer.id] == null && racerCount !== racersStarted) {
-                    racers[data.racer.id] = {};
-                    racersStarted++;
-                    if (racersStarted === racerCount) {
-                        ipc.server.broadcast('lap.start', )
-                    }
-                }
-            }
-        )
+        ipc.server.on('racer.init', registerRacer)
+        ipc.server.on('racer.position', updatePosition);
+
     }
 );
 
--- Racer.js
+++ Racer.js
@@ -1,27 +1,36 @@
 const ipc = require('node-ipc');
-
-ipc.config.id = `Racer ${process.argv[2]}`;
+const racer = {
+    id: process.argv[2],
+};
+ipc.config.id = `R${racer.id}`;
 ipc.config.retry = 15000;
 ipc.config.silent = true;
+
+function register() {
+    console.log(ipc.config.id + ': init');
+    console.log(ipc.config.id + ' connected to master');
+    ipc.of.master.emit('racer.init', racer);
+}
+
+function updateCoords() {
+    racer.coords[0] += 1;
+    ipc.of.master.emit('racer.position', {
+        id: racer.id,
+        coords: racer.coords,
+    });
+}
+
+function beginLap(data) {
+    racer.coords = getIntersection(data.lines);
+    clearInterval(racer.interval);
+    racer.interval = setInterval(updateCoords, 50);
+}
 
 ipc.connectTo(
     'master',
     function() {
-        ipc.of.master.on(
-            'connect',
-            function(){
-                console.log('## connected to master ##');
-                ipc.of.master.emit(
-                    'racer.init',  //any event or message type your server listens for
-                    {racer: {id: process.argv[2]}},
-                );
-            }
-        );
-        ipc.of.master.on(
-            'disconnect',
-            function() {
-                ipc.log('disconnected from world');
-            }
-        );
+        ipc.of.master.on('connect', register);
+        ipc.of.master.on('lap.begin', beginLap);
+        ipc.of.master.on('disconnect', process.exit());
     }
 )
